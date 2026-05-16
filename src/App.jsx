6e4f6bfeca2f3b211772e34c9578e82419449a71
Abdiaziz Mohamed <abdiazizmohdabdi@gmail.com>
2026-05-16 05:46:13 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -629,8 +629,10 @@ function App() {
   return (
 
     <div className={`
-      flex min-h-screen
-      overflow-hidden
+      flex
+      min-h-screen
+      w-full
+      overflow-x-hidden
 
       ${
         dark
@@ -648,19 +650,22 @@ function App() {
         currentUser={
           currentUser
         }
-        dark={dark}
       />
 
-      {/* MAIN */}
+      {/* MAIN CONTENT */}
       <main className="
-        flex-1 flex flex-col
-        h-screen overflow-hidden
+        flex-1
+        flex
+        flex-col
+
+        min-h-screen
+        w-full
+
+        overflow-x-hidden
       ">
 
         {/* TOPBAR */}
         <Topbar
-          dark={dark}
-          setDark={setDark}
           setAuthed={
             setAuthed
           }
@@ -673,12 +678,16 @@ function App() {
           medicines={
             medicines
           }
+          sales={sales}
         />
 
-        {/* PAGE */}
+        {/* PAGE CONTENT */}
         <div className="
-          flex-1 overflow-y-auto
+          flex-1
+          overflow-y-auto
           overflow-x-hidden
+
+          p-2 md:p-4
         ">
 
           {renderPage()}
